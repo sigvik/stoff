32b8706392660412f50b4ab9ef006b8c18000c5b
Sigurd <banansvele@gmail.com>
2022-08-04 17:02:43 +0200

    started search

--- template-parts/menu-overlay.php
+++ template-parts/menu-overlay.php
@@ -7,6 +7,7 @@
         <div class="icons">
             <button id="search-btn" class="icon">&#xe8b6;</button>
             <input id="search-input" type="text" placeholder="Søk.."></input><?php 
+            // get_search_form();
             if (get_theme_mod('ui_toggle')): ?>  
               <button id="ui-mode-toggle" class="icon">&#xe51c;</button><?php
             endif; ?>
