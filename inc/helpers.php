7af48a44eb7ccf3d4b429b2f7896ba41f16744fc
Sigurd <banansvele@gmail.com>
2022-08-14 17:16:59 +0200

    etest

--- inc/helpers.php
+++ inc/helpers.php
@@ -3,7 +3,7 @@
 
 
 // Add material symbol syntax to their code points
-function m_symbol($code) { return '<span>&#x'.$code.';</span>'; }
+function m_symbol($code) { return '<span>&#x'.$code.';&#xFE0E;</span>'; }
 
 // Shorter function for rendering an article preview, as it is done often
 function sak($args) { get_template_part('template-parts/sak', false, $args); }
