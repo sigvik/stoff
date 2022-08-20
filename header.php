9c395411351e4c237fe67d83161187a2170a9202
Sigurd <banansvele@gmail.com>
2022-08-20 12:34:50 +0200

    analytics

--- header.php
+++ header.php
@@ -11,6 +11,28 @@ require get_template_directory() . '/inc/directories.php';
 
 	<link rel="pingback" href="<?php echo $templURI . "/xmlrpc.php" ?>"  />
 
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-62250971-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-62250971-1');
+  </script>
+
+  <!-- Google Analytics -->
+  <script>
+    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('send', 'pageview');
+  </script>
+  <script async src="https://www.google-analytics.com/analytics.js"></script>
+  <!-- <script async src="path/to/autotrack.js"></script> -->
+  <!-- End Google Analytics -->
+
+
 	<?php wp_head(); ?>
 
   <?php // require $templDir . '/inc/category-clr.php' ?>
